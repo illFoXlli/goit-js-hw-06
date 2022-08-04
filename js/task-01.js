4d25543d4882f28922c5665f9106b83ccc3fd87b
Denis <fox.ruden@gmail.com>
2022-08-05 00:25:34 +0300

    end day

---
+++ js/task-01.js
@@ -0,0 +1,26 @@
+'use strict';
+// Напиши скрипт который:
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const classItem = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${classItem.length}`);
+
+classItem.forEach(item => {
+  const nameTitle = item.firstElementChild.textContent;
+  const listLength = item.lastElementChild.children.length;
+  console.log('Category: ' + nameTitle + '\n' + 'Elements: ' + listLength);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,22 @@
+// В HTML есть пустой список ul#ingredients.
+
+// <ul id="ingredients"></ul>
+// В JavaScript есть массив строк.
+
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +25,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elementIngredients = document.querySelector('#ingredients');
+
+ingredients.forEach(valio => {
+  const elementLi = document.createElement('li');
+  elementLi.textContent = valio;
+  elementLi.classList.add('item');
+  elementIngredients.append(elementLi);
+});
