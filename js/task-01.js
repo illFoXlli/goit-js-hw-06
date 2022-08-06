6732c6cc801af5e15d0b0c6df09600e7cdddc702
Denis <fox.ruden@gmail.com>
2022-08-06 15:39:12 +0300

    save06.08.2022

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,4 @@
 'use strict';
-// Напиши скрипт который:
-// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
-// В результате, в консоли будут выведены такие сообщения.
-
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
-
 const classItem = document.querySelectorAll('.item');
 
 console.log(`Number of categories: ${classItem.length}`);
--- js/task-02.js
+++ js/task-02.js
@@ -1,36 +1,20 @@
-// В HTML есть пустой список ul#ingredients.
-
-// <ul id="ingredients"></ul>
-// В JavaScript есть массив строк.
-
-// const ingredients = [
-//   "Potatoes",
-//   "Mushrooms",
-//   "Garlic",
-//   "Tomatos",
-//   "Herbs",
-//   "Condiments",
-// ];
-// Напиши скрипт, который для каждого элемента массива ingredients:
-
-// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
-// Добавит название ингредиента как его текстовое содержимое.
-// Добавит элементу класс item.
-// После чего вставит все <li> за одну операцию в список ul#ingredients.
+'use strict';
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
 
 const elementIngredients = document.querySelector('#ingredients');
-
+const arrayAddLiItem = [];
 ingredients.forEach(valio => {
-  const elementLi = document.createElement('li');
-  elementLi.textContent = valio;
-  elementLi.classList.add('item');
-  elementIngredients.append(elementLi);
+    const elementLi = document.createElement('li');
+    elementLi.textContent = valio;
+    elementLi.classList.add('item');
+
+    arrayAddLiItem.push(elementLi);
 });
+elementIngredients.append(...arrayAddLiItem);
